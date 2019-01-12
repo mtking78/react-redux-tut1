b31b7af31df4e2153ba3263eb15e42df58dfc8fa
mtking78 <mtking78@gmail.com>
2019-01-11 20:16:17 -0700

    Part 9 - Re-render React by subscribing to the React store.

--- src/index.js
+++ src/index.js
@@ -24,24 +24,31 @@ import STORY_ARCHIVE from './constants/actionTypes';
 //   },
 // ];
 
-ReactDOM.render(
-  // An archiving function can be passed down to the Story component from your src/index.js file. In the beginning, it can be an empty function. The function will be replaced later when you will dispatch a Redux action.
-  // <App stories={stories} onArchive={() => {}} />,
+function render() {
+  ReactDOM.render(
+    // An archiving function can be passed down to the Story component from your src/index.js file. In the beginning, it can be an empty function. The function will be replaced later when you will dispatch a Redux action.
+    // <App stories={stories} onArchive={() => {}} />,
 
-  // Instead of using the sample stories, you will use the stories that are stored in the Redux store. Taken that the store only saves a list of stories as state, you can simply get all the global state of the store and assume that it is the list of stories.
+    // Instead of using the sample stories, you will use the stories that are stored in the Redux store. Taken that the store only saves a list of stories as state, you can simply get all the global state of the store and assume that it is the list of stories.
 
-  //  Since the state is sliced up into two substates now, you have to adjust how you retrieve the stories from your store in the src/index.js file with the intermediate storyState now. This is a crucial step, because it shows how combined reducers slice up the state into substates.
-  <App
-    // stories={store.getState().storyState}
-    // Use the selector to compute the not-archived stories instead of retrieving the whole list of stories from the store.
-    stories={getReadableStories(store.getState())}
-    // Dispatch the action in your React entry point where you had the empty function before. Now the passed onArchive() function will dispatch an action when it is used.
-    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
-  />,
-  document.getElementById('root')
-);
+    //  Since the state is sliced up into two substates now, you have to adjust how you retrieve the stories from your store in the src/index.js file with the intermediate storyState now. This is a crucial step, because it shows how combined reducers slice up the state into substates.
+    <App
+      // stories={store.getState().storyState}
+      // Use the selector to compute the not-archived stories instead of retrieving the whole list of stories from the store.
+      stories={getReadableStories(store.getState())}
+      // Dispatch the action in your React entry point where you had the empty function before. Now the passed onArchive() function will dispatch an action when it is used.
+      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+    />,
+    document.getElementById('root')
+  );
+}
+
 // ReactDOM.render(<App />, document.getElementById('root'));
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
+// The components will re-render once you archive a story, because the state in the Redux store updates and the subscription will run to render again the whole component tree. In addition, you render the component once when the application starts.
+store.subscribe(render);
+render();
