cb4bc1bc1480453b19fcc72b3e7b334a4cffe417
mtking78 <mtking78@gmail.com>
2019-01-13 08:10:46 -0700

    Moved api from /sagas to /api folder.

--- src/sagas/story.js
+++ src/sagas/story.js
@@ -1,11 +1,6 @@
 import { call, put } from 'redux-saga/effects';
 import { doAddStories } from '../actions/story';
-
-const HN_BASE_URL = 'http://hn.algolia.com/api/v1/search?query=';
-
-const fetchStories = query =>
-  fetch(HN_BASE_URL + query)
-  .then(response => response.json());
+import { fetchStories } from '../api/story';
 
 // In your handleFetchStories() generator function, that is used in your root saga, you can use the yield statement to write asynchronous code as it would be synchronous code. As long as the promise from the Hacker News request doesn’t resolve (or reject), the next line of code after the yield state will not be evaluated.
 function* handleFetchStories(action) {
